5969077c9773f6e94bf3bf56dfd7c46fb226c853
Yago <dolmayan@gmail.com>
2024-10-30 23:42:09 +0100

    Update Player class with all data necessary to work

--- Player.rb
+++ Player.rb
@@ -12,43 +12,38 @@ class Player
   end
 
   # Pick a square, if empty, update with player symbol
-  def pick_square(row, col, board_obj)
-    green_light = false
+  def pick_square(board_obj)
+    
+    green_light = nil
     while green_light != true
+      green_light = false
       # prompt
-      player_x = nil
+      player_x = -1
       while player_x < 0 || player_x > 2
         puts "Pick a row between 0 and 2 (x coordinate)"
         player_x = gets.chomp
-        
-        if player_x == nil
-          continue
-        else
-          player_x = player_x.to_i
-        end
+        player_x = player_x.to_i
       end
 
-      player_y = nil
-      while player_y < 0 || player_x > 2
+      player_y = -1
+      while player_y < 0 || player_y > 2
         puts "Pick a column between 0 and 2 (y coordinate)"
         player_y = gets.chomp
-
-        if player_y == nil
-          continue
-        else
-          player_y = player_y.to_i
-        end
+        player_y = player_y.to_i
       end  
-    end
+
+      
     
-    # call Board.update_board. If return value is true, Board will update the grid
-    if board_obj.update_board(player_x, player_y, player_symbol) == true
-      green_light = true
-      # successful pick, update picks array 
-      picks.push([player_x, player_y])
-    else 
-      # while loop will continue reprompting until an empty square is found
-      green_light = false
+    
+      # call Board.update_board. If return value is true, Board will update the grid
+      if board_obj.update_board(player_x, player_y, @player_symbol) == true
+        green_light = true
+        # successful pick, update picks array 
+        @picks.push([player_x, player_y])
+      else 
+        # while loop will continue reprompting until an empty square is found
+        green_light = false
+      end
     end
   end
 end
