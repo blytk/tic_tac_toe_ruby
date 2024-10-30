b074961d24acd4bcab3d5c90cb04c351b91a766d
Yago <dolmayan@gmail.com>
2024-10-30 23:41:39 +0100

    Update Board class with all variables and methods necessary to work

--- Board.rb
+++ Board.rb
@@ -4,39 +4,69 @@ class Board
 
   def initialize()  
     @size = 3
-    @grid = Array.new(self.size) {Array.new(self.size)}
+    @grid = Array.new(self.size) {Array.new(self.size, " ")}
   end
   # Update board on coordinate row, col with symbol
   def update_board(row, col, symbol)
-    self.grid[row][col] = symbol
+    # empty? true if square is available, false if not
+    if self.grid[row][col] != "X" && self.grid[row][col] != "O"
+      self.grid[row][col] = symbol
+      return true
+    else
+      return false
+    end
+   
   end
   # Display entire board with current state
   def display_board
+    puts "======="
     self.grid.each_with_index do |row, row_index|
+      print "|"
       row.each_with_index do |col, col_index|
         print self.grid[row_index][col_index]
+        print "|"
       end
-      puts
+      puts ""
     end
+    puts "======="
+  end
+
+  def board_full?    
+    !self.grid.flatten.include?(" ")
+  end
+
+  def winner?
+    winner = false
+    # winning conditions
+    @grid.each_with_index do |row, row_index|
+      # entire row has the same symbol
+      if @grid[row_index][0] == @grid[row_index][1] && @grid[row_index][0] == @grid[row_index][2]
+        if @grid[row_index][0] != " "
+          winner = true
+          return winner
+        end
+      end
+    end
+    # entire column has the same symbol
+    for i in 0..2
+      if @grid[0][i] == @grid[1][i] && @grid[0][i] == grid[2][i] 
+        if @grid[0][i] != " "
+          winner = true
+          return winner
+        end
+      end
+    end
+
+    # diagonal has the same symbol
+    if @grid[0][0] == @grid[1][1] && @grid[0][0] == @grid[2][2] ||  \
+      @grid[2][0] == @grid[1][1] && @grid[2][0] == grid[0][2]
+      if @grid[1][1] != " "
+        winner = true
+        return winner
+      end
+    end
+      winner
   end
 end
 
-board = Board.new()
-
-=begin
-test_grid = [
-  [nil, nil, nil],
-  ["x" , "x", "o"],
-  ["o" , "o", "x"]
-]
-
-
-test_grid.each_with_index do |row, row_index|
-  row.each_with_index do |col, col_index|
-    board.update_board(row_index, col_index, test_grid[row_index][col_index])
-  end
-end
-
-=end
-board.display_board()
 
